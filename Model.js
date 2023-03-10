0876f309bd20443e9061d698c71b0baa460e308e
Klim <xarkoni@gmail.com>
2023-03-10 11:22:50 +0300

    model emj

--- Model.js
+++ Model.js
@@ -1,7 +1,20 @@
+const fs = require('fs').promises;
+const path1 = `${__dirname}/topics`;
 class Model {
-  constructor() {
+  constructor(path) {
+    this.path = path;
+  }
 
+  async checkDir() {
+    await fs
+      .readdir(this.path)
+      .then((data) => data.map((el) => `👉${el.slice(0, -4)}`))
+      .then((data) => data.forEach((el) => console.log(el)));
   }
 }
 
-module.exports = Model
+let test = new Model(path1);
+
+test.checkDir();
+
+module.exports = Model;
